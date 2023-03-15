c6690f3f445fcb2c8d5c6488b3d8f7dc49660981
yujin <14258164234@qq.com>
2023-03-15 18:01:29 +0800

    feature: 添加markdown全局样式

--- playground/index.js
+++ playground/index.js
@@ -1,31 +1,42 @@
 // import hljs from "highlight.js";
 
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
-
-import markdownParse from '../dist/index.js'
 import hljs from 'highlight.js'
+
+import { template } from './templateText.js'
+import markdownParse from '../dist/index.js'
+
+import '../styles/index.css'
 
 const userInputEl = document.querySelector('#userInput')
 const parseResultEl = document.querySelector('#parseResult')
+
+async function handle (value) {
+  const ast = await markdownParse.parse(value)
+  console.log('ast', ast)
+
+  const htmlAst = markdownParse.transform(ast, {
+    highlight: (code, lang) => {
+      if (hljs.getLanguage(lang)) {
+        return hljs.highlight(code, { language: lang }).value
+      }
+      return code
+    }
+  })
+  console.log('htmlAst', htmlAst)
+
+  const html = markdownParse.generate(htmlAst).value
+  console.log('html', html)
+
+  parseResultEl.innerHTML = html
+}
+
 async function onChange (e) {
   if (e.key === 'Enter') {
-    const ast = await markdownParse.parse(e.currentTarget.value)
-    console.log('ast', ast)
-
-    const htmlAst = markdownParse.transform(ast, {
-      highlight: (code, lang) => {
-        if (hljs.getLanguage(lang)) {
-          return hljs.highlight(code, { language: lang }).value
-        }
-        return code
-      }
-    })
-    console.log('htmlAst', htmlAst)
-
-    const html = markdownParse.generate(htmlAst).value
-    console.log('html', html)
-
-    parseResultEl.innerHTML = html
+    handle(e.currentTarget.value)
   }
 }
 userInputEl.addEventListener('keyup', onChange)
+
+userInputEl.textContent = template
+handle(userInputEl.textContent)
