c501bf7d0bb5e30dc09af921e481ca7345725984
yujin <14258164234@qq.com>
2023-03-15 15:16:36 +0800

    fix: 修复代码块解析贪婪模式导致异常

--- src/parse/codeBlock.ts
+++ src/parse/codeBlock.ts
@@ -3,27 +3,27 @@ import { ParseContext } from "./../types/index.d";
 import { advanceBy } from "./parse.js";
 
 export function parseCodeBlock(context: ParseContext, ancestors: MdAst[]) {
-	// CodeBlock State
-	const pattern = /^\`{3}([a-zA-Z]*)\n((.*\n*)*)\`{3}/;
-	const matchText = context.source.match(pattern);
+  // CodeBlock State
+  const pattern = /^\`{3}([a-zA-Z]*)\n((.*\n*)*?)\`{3}/;
+  const matchText = context.source.match(pattern);
 
-	if (!matchText) {
-		return;
-	}
+  if (!matchText) {
+    return;
+  }
 
-	const codeLanguage = matchText[1] || "plain";
-	const codeText = matchText[2];
+  const codeLanguage = matchText[1] || "plain";
+  const codeText = matchText[2];
 
-	const codeBlockAst: MdAst = {
-		type: "CodeBlock",
-		rowType: RowType.Block,
-		children: [],
-		meta: {
-			language: codeLanguage,
-		},
-		value: codeText,
-	};
+  const codeBlockAst: MdAst = {
+    type: "CodeBlock",
+    rowType: RowType.Block,
+    children: [],
+    meta: {
+      language: codeLanguage,
+    },
+    value: codeText,
+  };
 
-	advanceBy(context, matchText[0].length);
-	return codeBlockAst;
+  advanceBy(context, matchText[0].length);
+  return codeBlockAst;
 }
